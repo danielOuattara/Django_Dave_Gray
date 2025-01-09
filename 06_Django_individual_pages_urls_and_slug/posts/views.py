e2e0adc68c13e9556a614118ec7fa70a9b12ebd8
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2025-01-09 15:01:50 +0100

    06 Django single page, urls and slug: updated logic & `READ_ME_BUILD.MD`

--- 06_Django_individual_pages_urls_and_slug/posts/views.py
+++ 06_Django_individual_pages_urls_and_slug/posts/views.py
@@ -5,9 +5,9 @@ from .models import Post
 
 def posts_list(request):
     posts = Post.objects.all().order_by('-date')
-    return render(request, 'posts/posts_list.html', {'posts': posts})
+    return render(request, 'posts/posts_list.html', context={'posts': posts})
 
 
 def post_page(request, slug):
     post = Post.objects.get(slug=slug)
-    return render(request, 'posts/single-post.html', {'post': post})
+    return render(request, 'posts/single-post.html', context={'post': post})
